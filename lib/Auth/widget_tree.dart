a9d7241e2ae11647c434a66662a8fc42fe45ded0
“hadesgleizos” <mickakelove123164@gmail.com>
2024-06-13 15:11:24 +0800

    help and about update

--- lib/Auth/widget_tree.dart
+++ lib/Auth/widget_tree.dart
@@ -4,29 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:main/Auth/login_screen.dart';
 import 'Verification.dart';
 
-
 class testWidgetTree extends StatefulWidget {
   const testWidgetTree({Key? key}) : super(key: key);
-  
+
   @override
   State<testWidgetTree> createState() => _testWidgetTreeState();
 }
 
-class _testWidgetTreeState extends State<testWidgetTree>{
+class _testWidgetTreeState extends State<testWidgetTree> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: Auth().authStateChanges,
-        builder: (context, snapshot){
-          if (snapshot.hasData == false){
-            return Login();
-          } else {
-            if (snapshot.data?.emailVerified == true) { //set to false to bypass verification
-              return MyHomePage();
-            }
-            return const Verification();
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          return Login();
+        } else {
+          if (snapshot.data?.emailVerified == true) {
+            //set to false to bypass verification
+            return MyHomePage();
           }
-    },
+          return const Verification();
+        }
+      },
     );
   }
 }
--- lib/Home/about.dart
+++ lib/Home/about.dart
@@ -6,12 +6,62 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          Color(0xFFF1F1F1), // Set the background color of the entire screen
+      backgroundColor: Color(0xFFF1F1F1), // Set the background color of the entire screen
       appBar: AppBar(
         backgroundColor: Colors.transparent, // Make app bar transparent
         elevation: 0, // Remove app bar shadow
-        title: Text('About'),
+        title: Text('About', style: TextStyle(color: Colors.black)),
+        iconTheme: IconThemeData(color: Colors.black), // Change back button color to black
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'About the App',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'This app provides users with a comprehensive set of tools for managing their recipes and scanning ingredients.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Team',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'We are college students who developed this project for our software engineering course.\n\n'
+                  'Charles Vincent A Fernando\n'
+                  'Orland II P. Geronimo\n'
+                  'Pocholo A. Granada',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Contact Information',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
       ),
     );
   }
